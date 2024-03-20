ae223f74ce93447dc2a87297ac0c8c36ed436f95
JuanK <juancamilo.manjarres@gmail.com>
2024-03-19 23:03:53 -0500

    refactor

---
+++ back/controllers/motos.controller.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const motosRouter = express.Router();
+const {
+  getMotos,
+  createMoto,
+  updateMoto,
+  leavingMoto,
+  deleteMoto,
+} = require("../services/moto.service");
+
+// define the home page route
+motosRouter.get("/", async (req, res) => {
+  const data = await getMotos();
+  res.json({ data: data });
+});
+
+motosRouter.post("/create", async (req, res) => {
+  const newData = req.body;
+  await createMoto(newData);
+  res.json({ message: "Data created successfully!", data: newData });
+});
+
+motosRouter.post("/update", async (req, res) => {
+  const data = await updateMoto(req.body);
+  res.json({ message: "Data updated successfully!", data: data });
+});
+motosRouter.post("/leaving", async (req, res) => {
+  const data = await leavingMoto(req.body.placa);
+  res.json({ message: "Moto leaved successfully!", data: data });
+});
+motosRouter.post("/delete", async (req, res) => {
+  const placa = req.body.placa;
+  const data = await deleteMoto(placa);
+  res.json({ message: "Data deleted successfully!", data: data });
+});
+
+module.exports = motosRouter;
---
+++ back/services/moto.service.js
@@ -0,0 +1,137 @@
+const db = require("../firebase.js");
+const {
+  collection,
+  addDoc,
+  query,
+  where,
+  updateDoc,
+  getDoc,
+  getDocs,
+  doc,
+  limit,
+  deleteDoc,
+} = require("firebase/firestore");
+
+async function getMotos() {
+  try {
+    const documents = {};
+    const querySnapshot = await getDocs(collection(db, "motos"));
+    querySnapshot.forEach((doc) => {
+      documents[doc.id] = doc.data();
+    });
+    return documents;
+  } catch (e) {
+    console.error("Error fetching motos", e);
+  }
+}
+
+async function createMoto({ placa, modelo, celda }) {
+  try {
+    const now = new Date();
+    const docRef = await addDoc(collection(db, "motos"), {
+      placa: placa,
+      modelo: modelo,
+      horaIngreso: now,
+      celda: celda,
+      estado: "INSIDE",
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+async function updateMoto({ placa, modelo, celda }) {
+  try {
+    const collectionRef = collection(db, "motos");
+    const q = query(
+      collectionRef,
+      where("placa", "==", placa),
+      where("estado", "==", "INSIDE"),
+      limit(1)
+    );
+    const querySnapshot = await getDocs(q);
+    const docId = querySnapshot.docs[0].id;
+    const docRef = doc(db, "motos", docId);
+    return await updateDoc(docRef, {
+      placa: placa,
+      modelo: modelo,
+      celda: celda,
+    });
+  } catch (e) {
+    console.error("Error Updating Moto", e);
+  }
+}
+
+async function leavingMoto(placa) {
+  let difference;
+  let endDate;
+  try {
+    const collectionRef = collection(db, "motos");
+    const q = query(
+      collectionRef,
+      where("placa", "==", placa),
+      where("estado", "==", "INSIDE"),
+      limit(1)
+    );
+    const querySnapshot = await getDocs(q);
+    const docId = querySnapshot.docs[0].id;
+    const docRef = doc(db, "motos", docId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      // console.log("Document data:", docSnap.data());
+      const startDate = new Date(docSnap.data().horaIngreso.seconds * 1000);
+      endDate = new Date();
+      difference = calculateTimeDifference(startDate, endDate);
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+      return "No moto with placa";
+    }
+
+    await updateDoc(docRef, {
+      estado: "LEAVE",
+      horaSalida: endDate,
+      precio: difference.hours * 1000, //1000 pesos por hora
+    });
+    const updatedDoc = await getDoc(docRef);
+    return updatedDoc.data();
+  } catch (e) {
+    console.error("Error Updating Moto", e);
+  }
+}
+
+async function deleteMoto(placa) {
+  try {
+    const collectionRef = collection(db, "motos");
+    const q = query(collectionRef, where("placa", "==", placa), limit(1));
+    const querySnapshot = await getDocs(q);
+    const docId = querySnapshot.docs[0].id;
+    const docRef = doc(db, "motos", docId);
+    return await deleteDoc(docRef);
+  } catch (e) {
+    console.error("Error Updating Moto", e);
+  }
+}
+
+function calculateTimeDifference(startDate, endDate) {
+  // Ensure valid Date objects
+  if (!(startDate instanceof Date) || !(endDate instanceof Date)) {
+    throw new Error("Invalid date objects provided");
+  }
+
+  // Get the difference in milliseconds
+  const timeDifference = endDate.getTime() - startDate.getTime();
+
+  // Convert milliseconds to hours, minutes, and seconds
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  // Return an object with the time components
+  return { hours, minutes, seconds };
+}
+
+module.exports = { getMotos, createMoto, updateMoto, leavingMoto, deleteMoto };
