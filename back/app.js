6bc92af53620ca66b7de40159c2b818ad8992260
JuanK <juancamilo.manjarres@gmail.com>
2024-03-20 23:37:36 -0500

    feat: fetch motos

--- back/app.js
+++ back/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const motosRouter = require('./controllers/motos.controller.js')
+const cors = require('cors');
 
 //Initialize express app
 const app = express();
 const port = 3000;
+app.use(cors());
 
 app.use(express.json());
 
--- front/src/app/listar/page.js
+++ front/src/app/listar/page.js
@@ -1,3 +1,56 @@
-export default function Home() {
-    return <></>;
-  }
+"use client";
+import { useEffect, useState } from "react";
+
+export default function ListarMotos() {
+  const [motos, setMotos] = useState(null);
+  const [fetched, setFetched] = useState(null);
+  const getData = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}`);
+    const data = await response.json();
+    setMotos(data.data);
+    setFetched(true);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <main className="flex flex-col ml-10 mr-10">
+      <h1 className="text-3xl">Motos listing</h1>
+      {fetched && (
+        <section className="flex w-full h-full">
+          <ul className="flex flex-col gap-5">
+            {Object.keys(motos).map((motoId, index) => (
+              <li key={motoId}>
+                <MotoDetails moto={motos[motoId]} />
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
+    </main>
+  );
+}
+
+function MotoDetails({ moto }) {
+  return (
+    <div>
+      <h2 className="text-xl">Moto Details</h2>
+      <p>
+        <b>Estado:</b> {moto.estado}
+      </p>
+      <p>
+        <b>Modelo:</b> {moto.modelo}
+      </p>
+      <p>
+        <b>Celda:</b> {moto.celda}
+      </p>
+      <p>
+        <b>Hora de Ingreso:</b>
+        {new Date(moto.horaIngreso.seconds * 1000).toLocaleString()}
+      </p>
+      <p>
+        <b>Placa:</b> {moto.placa}
+      </p>
+    </div>
+  );
+}
