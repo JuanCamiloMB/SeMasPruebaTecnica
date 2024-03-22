75a5490f0db989fdf36066df0140ecef7539e452
JuanK <juancamilo.manjarres@gmail.com>
2024-03-22 14:38:52 -0500

    fix: Port error fixed

--- back/app.js
+++ back/app.js
@@ -12,6 +12,21 @@ app.use(express.json());
 
 app.use("/", motosRouter);
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+function startServer(port) {
+  app
+    .listen(port, () => {
+      console.log(`App running on port ${port}`);
+    })
+    .on("error", (err) => {
+      if (err.code === "EADDRINUSE") {
+        console.log(
+          `El puerto ${port} está ocupado, intentando otro puerto...`
+        );
+        startServer(port + 1);
+      } else {
+        console.error(err);
+      }
+    });
+}
+
+startServer(port)
